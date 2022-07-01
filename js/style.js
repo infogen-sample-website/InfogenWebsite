dff081e4ca6622de3b5fe4a31b4e4a3fded27c95
infogen-sample-website <108496002+infogen-sample-website@users.noreply.github.com>
2022-07-01 12:24:55 +0530

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -1,158 +1,142 @@
- var slide_index = 1;
+var slide_index = 1;
 displaySlides(slide_index);
 
 function nextSlide(n) {
-    displaySlides((slide_index += n));
+  displaySlides((slide_index += n));
+}
+
+function displaySlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("showSlide");
+  if (n > slides.length) {
+    slide_index = 1;
   }
-  
-  
-  function displaySlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("showSlide");
-    if (n > slides.length) {
-      slide_index = 1;
-    }
-    if (n < 1) {
-      slide_index = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    slides[slide_index - 1].style.display = "block";
+  if (n < 1) {
+    slide_index = slides.length;
   }
-   
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slide_index - 1].style.display = "block";
+}
+
 /* What we do slider */
 
 var swiper = new Swiper(".mySwiper", {
-    loop:true,
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false
+  loop: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-    slidesPerView: 1,
-    spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      "@0.00": {
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      "@0.75": {
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      "@1.00": {
-        slidesPerView: 3,
-        spaceBetween: 40,
-      },
-      "@1.50": {
-        slidesPerView: 4,
-        spaceBetween: 50,
-      },
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
-  });
-
-
-
-  /* Typing Animation */
-
-  /* var typed = new typed(".typing",{
-    strings:["EXTENSIVE EXPERIENCE","Infogen"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-  }); */
-
-
-
-
-/* Nav Menu item active when selected */
-const nav = document.querySelector(".nav"),
-      navList = document.querySelectorAll("li"),
-      totalNavList = navList.length;
-      console.log("Nav length is ====>"+totalNavList);
-      for(let i=0;i<totalNavList;i++)
-      {
-        
-        const a = navList[i].querySelector("a");
-
-        a.addEventListener("click",function(){
-          console.log(this);
-          for(let j=0;j<totalNavList;j++){
-            navList[j].querySelector("a").classList.remove("active");
-          }
-          this.classList.add("active");
-        });
-        
-      }
-
-/* -------------------------------------------------------- */
-
-
-
-
-
-
-
-
-
-
-
-  /* Toggler SIdebar */
-
- /*  const navTogglerBtn = document.querySelector(".nav-toggler"),
-  sidebar = document.querySelector(".sidebar");
-
-  navTogglerBtn.addEventListener("click", ()=>{
-    sidebarSectionTogglerBtn();
-  })
-
-
-  function sidebarSectionTogglerBtn(){
-    sidebar.classList.toggle("open");
-  } */
-
-
-
-  /* Sidebar function */
-
-  /* Sidebar functions */
-
-/* $("#studios_btn").on( "click", function() {
-  $('.sidebar .nav .studios_show').toggleClass('show');
-}); */
-/* 
-$("#about_btn").on( "click", function() {
-  $('.sidebar_section .sidebar .menu li ul.about_show').toggleClass('show');
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
 });
-
-$("#whatwedo_btn").on( "click", function() {
-  $('.sidebar_section .sidebar .menu li ul.whatwedo_show').toggleClass('show');
-}); */
 
 
 /* Toggle Sidebar  */
 const togglebtn = document.getElementById("toggle"),
-      sidebar = document.getElementById("sidebar"),
-      removebtn =document.getElementById("removebtn");
- 
-      /* display sidebar */
-      togglebtn.addEventListener("click",function(){
-          sidebar.style.left="0";
-          togglebtn.style.display="none";
-         /*  console.log("======"+removebtn.style.display="none");
-          */ removebtn.style.display="block";
-      });
+  sidebar = document.getElementById("sidebar"),
+  removebtn = document.getElementById("removebtn");
 
-      /* remove Sidebar */
-      removebtn.addEventListener("click",function(){
-        sidebar.style.left="-270px";
-        removebtn.style.display="none";
-        togglebtn.style.display="flex";
-      });
+/* display sidebar */
+togglebtn.addEventListener("click", function () {
+  sidebar.style.left = "0";
+  togglebtn.style.display = "none";
+  /*  console.log("======"+removebtn.style.display="none");
+   */ removebtn.style.display = "block";
+});
+
+/* remove Sidebar */
+removebtn.addEventListener("click", function () {
+  sidebar.style.left = "-270px";
+  removebtn.style.display = "none";
+  togglebtn.style.display = "flex";
+});
+
+/* -------------------------------------------------------- */
+
+/* Nav Menu item active when selected */
+const nav = document.querySelector(".nav"),
+  navList1 = document.querySelectorAll("li"),
+  totalNavList1 = navList1.length;
+/* console.log("Nav length is ====>"+totalNavList); */
+for (let i = 0; i < totalNavList1; i++) {
+  const a = navList1[i].querySelector("a");
+
+  a.addEventListener("click", function () {
+    /*  console.log(this); */
+    for (let j = 0; j < totalNavList1; j++) {
+      navList1[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
 
 
+/* highlight menu items on scroll  */
+const sections = document.querySelectorAll('section');
+const navli = document.querySelectorAll('.sidebar .nav li');
 
+console.log(sections);
+console.log(navli);
+
+window.addEventListener("scroll", () => {
+  
+  let current ='';
+    
+  sections.forEach( section =>{
+    const sectionTop = section.offsetTop; 
+    const sectionHeight = section.clientHeight;
+    if(pageYOffset >= (sectionTop - sectionHeight / 3))
+    {
+      current = section.getAttribute('id');
+    }
+
+    navli.forEach( li=> {
+      li.classList.remove('active');
+      if(li.classList.contains(current)){
+        li.classList.add('active')
+      }
+    })
+
+  
+  });
+
+  console.log(current);
+
+});
+
+
+/* Animate the Heading using Ajax*/
+
+var typing = new Typed(".texttype_typing", {
+  strings: [
+    "",
+    "Extensive Experience..."
+  ],
+  typeSpeed: 120,
+  backSpeed: 20,
+  loop: true,
+});
